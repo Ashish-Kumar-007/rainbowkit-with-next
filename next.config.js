1f5a28ef3a3f92c234bd7a6137941e47ad923282
Ash45# <ashishsahoo2015@gmail.com>
2023-08-24 15:26:53 +0530

    test-3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        fs: false,
+        tls: false,
+        net:false
+      },
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
